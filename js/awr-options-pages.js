5ba568b9e21ec4c833f3bd8d5b80b48511222ec9
Ryan Kienstra <kienstraryan@gmail.com>
2015-01-27 02:29:04 -0400

    awr-options-pages.js: moved functions into variable assignemtns, not named functions

--- js/awr-options-pages.js
+++ js/awr-options-pages.js
@@ -3,71 +3,79 @@
 
 		// Link to delete page
 		$( '.awr-delete-page' ).live( 'click' , function() {
-			var data_awr_target = $( this ).attr( 'data-awr-target' );
-			if ( ! data_awr_target ) {
-				data_awr_target = $( this ).data( 'awr-target' );
+			var pageToDelete ,
+			    dataAwrTarget = $( this ).attr( 'data-awr-target' );
+
+			if ( ! dataAwrTarget ) {
+				dataAwrTarget = $( this ).data( 'awr-target' );
 			}
-			page_to_delete = $( '#' + data_awr_target );
-			page_to_delete.css( 'display' , 'none' )
-				.val( '' );
+			pageToDelete = $( '#' + dataAwrTarget );
+			pageToDelete.css( 'display' , 'none' )
+				    .val( '' );
 			$( this ).css( 'display' , 'none' );
 			return false;
 		} );
 
 		// Link to create new page
 		$( '.awr-new-page' ).on( 'click' , function() {
-			var $awr_pages = $( '.awr-page-input' ) ,
-			    $last_page_input = $awr_pages.last() ,
-			    $element_before_new_button = get_element_before_new_button() ,
-			    new_page_number = get_new_page_number() ,
-			    new_page_input_id = 'awr-page-' + new_page_number ,
-			    $new_page_input = get_new_page_input() ,
-			    $new_page_delete_button = get_new_page_delete_button() ,
-			    $break_tags = $( '</br></br>' );
+			var getElementBeforeNewButton , getNewPageNumber ,
+			    getNewPageInput , getNewPageDeleteButton ,
+			    $elementBeforeNewButton , newPageNumber , newPageInputId ,
+			    $newPageInput , $newPageDeleteButton , $breakTags,
+			    $awrPages = $( '.awr-page-input' ) ,
+			    $lastPageInput = $awrPages.last();
 
-			[ $new_page_delete_button , $new_page_input , $break_tags ].map( function( element , index ) {
-				element.insertAfter( $element_before_new_button );
-			} );
-
-			function get_element_before_new_button() {
-				if ( $last_page_input.length === 0 ) {
+			getElementBeforeNewButton = function() {
+				if ( $lastPageInput.length === 0 ) {
 					return $( '.awr-options-form .form-table td' );
 				}
 				else {
-					return $last_page_input.next();
+					return $lastPageInput.next();
 				}
 			}
 
-			function get_new_page_number() {
-				if ( $last_page_input.length === 0 ) {
+			getNewPageNumber = function() {
+				if ( $lastPageInput.length === 0 ) {
 					return 1;
 				}
 				else {
-					var last_page_id = $last_page_input.attr( 'id' );
+					var last_page_id = $lastPageInput.attr( 'id' );
 					var last_page_number = Number( last_page_id.match( /awr-page-([\d]*)/ )[ 1 ] );
 					return last_page_number + 1;
 				}
 			}
 
-			function get_new_page_input() {
+			getNewPageInput = function() {
 				return $( '<input>' ).attr( {
-					type	: 'text' ,
-					id		: new_page_input_id ,
+					type  : 'text' ,
+					id    : newPageInputId ,
 					class : 'awr-page-input' ,
 					value : '' ,
-					name : 'map_awr_index_to_page_title[' + new_page_number + ']'
+					name  : 'map_awr_index_to_page_title[' + newPageNumber + ']'
 				} );
 			}
 
-			function get_new_page_delete_button() {
+			getNewPageDeleteButton = function() {
 				return $( '<a>' )
 					.attr( {
-						href : '#' ,
+						href  : '#' ,
 						class : 'button-secondary awr-delete-page' ,
-					} )
-					.data( 'awr-target' , new_page_input_id )
+					       }
+					     )
+					.data( 'awr-target' , newPageInputId )
 					.html( 'Delete' );
 			}
+
+			$elementBeforeNewButton = getElementBeforeNewButton();
+			newPageNumber = getNewPageNumber();
+			newPageInputId = 'awr-page-' + newPageNumber;
+			$newPageInput = getNewPageInput();
+			$newPageDeleteButton = getNewPageDeleteButton();
+			$breakTags = $( '</br></br>' );
+
+			[ $newPageDeleteButton , $newPageInput , $breakTags ].map( function( element , index ) {
+				element.insertAfter( $elementBeforeNewButton );
+			} );
 
 			return false;
 
