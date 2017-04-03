c83b3deebc2b55ee309afe206a07f817e1baea71
Ryan Kienstra <ryan.kienstra@xwp.co>
2017-04-03 00:26:22 -0300

    Instantiate the main plugin class.
    Following its refactoring as a class, instead of globale functions.
    Also, correct the plugin version in the main plugin file.

--- adapter-widget-rows.php
+++ adapter-widget-rows.php
@@ -12,7 +12,7 @@ namespace AdapterWidgetRows;
 Plugin Name: Adapter Widget Rows
 Plugin URI: www.ryankienstra.com/plugins/adapter-widget-rows
 Description: Drag and drop widgets into rows. To get started, click "Settings" and enter a page name. Find that page in the front of your site and add widgets.
-Version: 1.0.0
+Version: 1.0.1
 Author: Ryan Kienstra
 Author URI: www.ryankienstra.com
 License: GPLv2
@@ -22,4 +22,4 @@ License: GPLv2
 require_once dirname( __FILE__ ) . '/includes/class-adapter-widget-rows-plugin.php';
 
 global $adapter_widget_rows_plugin;
-//$adapter_widget_rows_plugin = new Adapter_Widget_Rows_Plugin();
+$adapter_widget_rows_plugin = new Adapter_Widget_Rows_Plugin();
